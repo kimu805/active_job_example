f26ac35e64ce5d2ddff657537029e6286fe1c98c
kimu805 <kimukimuok805@gmail.com>
2024-10-03 18:04:02 +0900

    Update async_log_job.rb

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -1,7 +1,16 @@
 class AsyncLogJob < ApplicationJob
-  queue_as :async_log
+  queue_as do
+    case self.arguments.first[:message]
+    when "to async_log"
+      :async_log
+    else
+      :default
+    end
+  end
 
   def perform(message: "hello")
     AsyncLog.create!(message: message)
   end
+
+  retry_on 
 end
